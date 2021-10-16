e1139808129016466a5cc4f642541a82d25da753
Oleg Izvolov <oleg-izvolov@yandex.ru>
2021-10-16 15:27:26 +0300

    L7: modified - views, serializers, models etc - add service.py

--- movies/views.py
+++ movies/views.py
@@ -1,14 +1,21 @@
+from django.db import models
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from .models import Movie
-from .serializers import MovieListSerializer, MovieDetailSerializer
+from .serializers import MovieListSerializer, MovieDetailSerializer, ReviewCreateSerializer, CreateRatingSerializer
+from .service import get_client_ip
+
 
 class MovieListView(APIView):
     """Вывод списка фильмов"""
 
     def get(self, request):
-        movies = Movie.objects.filter(draft=False)
+        movies = Movie.objects.filter(draft=False).annotate(
+            rating_user=models.Count("ratings", filter=models.Q(ratings__ip=get_client_ip(request)))
+        ).annotate(
+            middle_star=models.Sum(models.F('ratings__star')) / models.Count(models.F('ratings'))
+        )
         serializer = MovieListSerializer(movies, many=True)
         return Response(serializer.data)
 
@@ -19,3 +26,23 @@ class MovieDetailView(APIView):
         movie = Movie.objects.filter(id=pk, draft=False)
         serializer = MovieDetailSerializer(movie, many=True)
         return Response(serializer.data)
+
+class ReviewCreateView(APIView):
+    """Добавление отзыва к фильму"""
+
+    def post(self, request):
+        review = ReviewCreateSerializer(data=request.data)
+        if review.is_valid():
+            review.save()
+        return Response(status=201)
+
+class AddStarRatingView(APIView):
+    """Добавление рейтинга к фильму"""
+
+    def post(self, request):
+        serializer = CreateRatingSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save(ip=get_client_ip(request))
+            return Response(status=201)
+        else:
+            return Response(status=400)
